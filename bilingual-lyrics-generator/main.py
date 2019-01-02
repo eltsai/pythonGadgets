09fe41a78974cbff58b9e365144beb879532ac89
Elisa <etsai042@gmail.com>
2019-01-02 21:54:46 +0800

    a time axis generator

--- bilingual-lyrics-generator/main.py
+++ bilingual-lyrics-generator/main.py
@@ -12,15 +12,15 @@ if __name__ == '__main__':
         
         lines = t.get("1.0",'end-1c').splitlines()
         
-        for i in range((len(lines)-1)//2+1):
-            timeaxis = lines[2*i][:7]
+        for i in range((len(lines)-1)//2):
+            timeaxis = lines[2*i+2][:7]
             lines[2*i+1] = timeaxis + lines[2*i+1]
         t.delete("1.0",'end-1c')
         t.insert("1.0", "\n".join(lines))
         
     root = tk.Tk()
     root.title("Bi-Ly-Ge")
-    t = tk.Text(root, height=20, width=40)
+    t = tk.Text(root, height=20, width=80)
     t.pack()
     button = tk.Button(root, text='Add Time', width=20, command=generatelyrics)
     button.pack()
